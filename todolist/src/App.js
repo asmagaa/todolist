00661f6fb46843afcf0cb5fb3fd1d1741aecdff2
Aleksander Smaga <165085833+asmagaa@users.noreply.github.com>
2024-10-03 17:30:18 +0200

    Fix
    Added components and fixed some code.

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,42 +1,35 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import './App.css';
 
 function App() {
-	const [tasks, setTasks] = useState([])
-	const [inputValue, setInputValue] = useState('')
+  const [todos, setTodos] = useState([]);
 
-	const handleInputChange = e => {
-		setInputValue(e.target.value)
-	}
+  const addTodo = (text) => {
+    const newTodo = { id: Date.now(), text, completed: false };
+    setTodos([...todos, newTodo]);
+  };
 
-	const addTask = () => {
-		if (inputValue.trim()) {
-			setTasks([...tasks, inputValue])
-			setInputValue('')
-		}
-	}
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map(todo => 
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
 
-	const removeTask = index => {
-		const newTasks = tasks.filter((task, i) => i !== index)
-		setTasks(newTasks)
-	}
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
 
-	return (
-		<div className='App'>
-			<h1>to-do lista</h1>
-			<div>
-				<input type='text' value={inputValue} onChange={handleInputChange} placeholder='np: Stwórz todolistę...' />
-				<button onClick={addTask}>dodaj</button>
-			</div>
-			<ul>
-				{tasks.map((task, index) => (
-					<li key={index}>
-						{task} <button onClick={() => removeTask(index)}>usuń</button>
-					</li>
-				))}
-			</ul>
-		</div>
-	)
+  return (
+    <div className="App">
+      <h1 className="header">ToDo List</h1>
+      <AddTodo addTodo={addTodo} />  {}
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />  {}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ todolist/src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TodoItem({ todo, toggleTodo, deleteTodo }) {
+  return (
+    <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>  {}
+      <span onClick={() => toggleTodo(todo.id)} className="todo-text">
+        {todo.text}  {}
+      </span>
+      <button onClick={() => deleteTodo(todo.id)} className="delete-button">Delete</button>  {}
+    </li>
+  );
+}
+
+export default TodoItem;
